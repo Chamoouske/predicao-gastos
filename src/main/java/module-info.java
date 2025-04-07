9346af7e981bb579b63a205603e6bc2b57d199bc
Ajax Lima <ajaxlima94@gmail.com>
2025-04-07 19:40:41 -0300

    feat: commit inicial do projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.predicao.gastos.predicaogastos {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.predicao.gastos.predicaogastos to javafx.fxml;
+    exports com.predicao.gastos.predicaogastos;
+}
